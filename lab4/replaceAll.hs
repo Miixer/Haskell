0c562eb8c117e5b428408ffa0e9c79e317564d2a
Miixer <33287805+Miixer@users.noreply.github.com>
2019-12-01 20:13:00 +0000

    Update replaceAll.hs

--- lab4/replaceAll.hs
+++ lab4/replaceAll.hs
@@ -1,6 +1,6 @@
 -- replaces all occurrences of a given value with another in a list.
-e.g. replaceAll 2 4 [1,2,3,2,1]
-     = [1,4,3,4,1]
+-- e.g. replaceAll 2 4 [1,2,3,2,1]
+--      = [1,4,3,4,1]
 
 replaceAll :: Eq a => a -> a -> [a] -> [a]
 replaceAll old new [] = []

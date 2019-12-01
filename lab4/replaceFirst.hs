e253a23ba1e97c90a30c01f01bc9f8b82e8e23b1
Miixer <33287805+Miixer@users.noreply.github.com>
2019-12-01 19:34:59 +0000

    Create replaceFirst.hs

---
+++ lab4/replaceFirst.hs
@@ -0,0 +1,9 @@
+-- replaces the first occurrence of a given value with another in a list.
+e.g. replaceFirst 2 4 [1,2,3,2,1]
+     = [1,4,3,2,1]
+
+replaceFirst :: Eq a => a -> a -> [a] -> [a]
+replaceFirst old new [] = []
+replaceFirst old new (head:tail)
+ | (head == old ) = new:tail
+ | otherwise = head : replaceFirst old new tail
